f1cf503e2e2514e35361fa21ab51f094747788f3
Jungy <jungorend@gmail.com>
2022-01-29 09:56:07 -0500

    LFG now mentions the taregt

--- src/ballot/core.clj
+++ src/ballot/core.clj
@@ -248,9 +248,8 @@
                                       (some #(= (:id author) %) (mapv #(:id (first %)) (:lfg-queue @state))) (do (swap! state update :lfg-queue
                                                                                                                         (fn [queue] (filter #(not= (:id author) (:id (first %))) queue)))
                                                                                                                  (discord-rest/create-message! (:rest @state) channel-id :content "You have been removed from the queue."))
-                                      :else (do (discord-rest/create-message! (:rest @state) channel-id :content (str "Someone is available for a match! Please reach out to @"
-                                                                                                                      (:username (first (first (:lfg-queue @state))))
-                                                                                                                      "."))
+                                      :else (do (discord-rest/create-message! (:rest @state) channel-id :content (str "Someone is available for a match! Please reach out to "
+                                                                                                                      (mention-user (:id (first (first (:lfg-queue @state))))) "."))
                                                 (swap! state update :lfg-queue #(drop 1 %)))))
       :else nil)))
 
