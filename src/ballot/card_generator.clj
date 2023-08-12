af3ae8466380ebc60894eda8ec2db35a0008aafc
Jungy <jungorend@gmail.com>
2023-08-12 16:14:29 -0400

    Easier typing of abilities

--- src/ballot/card_generator.clj
+++ src/ballot/card_generator.clj
@@ -4,6 +4,38 @@
   [token]
   (clojure.string/join " " (map clojure.string/capitalize (rest (clojure.string/split (str token) #"-")))))
 
+(defn get-trigger! []
+  (println "Trigger:")
+  (let [trigger (read-line)]
+    (if (#{"now"
+           "passive"
+           "before"
+           "hit"
+           "after"
+           "cleanup"} trigger)
+      (keyword trigger)
+      (do (println "That isn't a valid trigger. Please try again.")
+          (recur)))))
+
+(defn get-location! []
+  (println "Location:")
+  (let [location (read-line)]
+    (if (or (= location "attack") (= location "a"))
+      :attack
+      :boost)))
+
+(defn get-abilities! [name]
+  (println (str "Please print the abilities for " name))
+  (loop [abilities []]
+    (println "Description: ")
+    (let [desc (read-line)]
+      (if (seq desc)
+        (recur (conj abilities
+                     #:ability {:description desc
+                                :location (get-location!)
+                                :trigger (get-trigger!)}))
+        abilities))))
+
 (defn create-cards
   [deck-name character season & cards]
   (let [create-deck-str
@@ -19,31 +51,28 @@
                              "             :gauge-cost X\n"
                              "             :seasons {:season/name \"" season "\"}}\n\n")
         card-strs (reduce (fn [r c]
-                            (str r
-                                 "#:card {:name \"" (de-key (first c)) "\"\n"
-                                 "        :type :special/:ultra\n"
-                                 "        :id " (first c) "\n"
-                                 "        :cost 0\n"
-                                 "        :min-range 0\n"
-                                 "        :max-range 0\n"
-                                 "        :power 0\n"
-                                 "        :speed 0\n"
-                                 "        :armor 0\n"
-                                 "        :guard 0\n"
-                                 "        :boost-name \"\"\n"
-                                 "        :boost-cost 0\n"
-                                 "        :boost-type :instant\n"
-                                 "        :abilities []}\n\n"))
+                            (let [name (de-key (first c))
+                                  abilities (get-abilities! name)
+                                  ppabilities (with-out-str (clojure.pprint/pprint abilities))]
+                              (str r
+                                   "#:card {:name \"" (de-key (first c)) "\"\n"
+                                   "        :type :special\n"
+                                   "        :id " (first c) "\n"
+                                   "        :cost 0\n"
+                                   "        :min-range 0\n"
+                                   "        :max-range 0\n"
+                                   "        :power 0\n"
+                                   "        :speed 0\n"
+                                   "        :armor 0\n"
+                                   "        :guard 0\n"
+                                   "        :boost-name \"\"\n"
+                                   "        :boost-cost 0\n"
+                                   "        :boost-type :instant\n"
+                                   "        :abilities " ppabilities "}\n\n")))
                           "" (partition 2 cards))]
     (str character-cards
          card-strs
          create-deck-str)))
 
-(spit "results.edn" (create-cards "Syrus" :s2-syrus "Seventh Cross"
-                                  :s2-albatross-talon 2
-                                  :s2-aria-of-the-wind 2
-                                  :s2-siren-call 2
-                                  :s2-tidal-whirl 2
-                                  :s2-treasure-hunter 2
-                                  :s2-dredge-fury 2
-                                  :s2-symphony-of-the-deep 2))
+(spit "results.edn" (create-cards "Faust" :s7-faust "Guilty Gear"
+                                  :s7-mix-mix-mix 2))
